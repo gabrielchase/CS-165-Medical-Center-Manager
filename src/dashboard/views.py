c515bcf0173db9916b786f262ef535f4e7c20328
gabrielchase <gchase.patron@gmail.com>
2017-11-04 17:39:54 +0800

    Add user context in dashboard

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -5,3 +5,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context['user'] = self.request.user
+        return context
