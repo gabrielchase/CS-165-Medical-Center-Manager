19fac09d3a36ceba34b45547453c37d8b2ee4acb
gabrielchase <gchase.patron@gmail.com>
2017-11-05 01:58:45 +0800

    Change template name in `DashboardView`

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
-    template_name = 'dashboard.html'
+    template_name = 'dashboard/home.html'
 
     def get_context_data(self, *args, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
