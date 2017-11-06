66498713ebac966a38a75c5a26aa0fc643b021e3
gabrielchase <gchase.patron@gmail.com>
2017-11-06 16:04:15 +0800

    Create `Service` and `Product` tables

--- src/dashboard/models.py
+++ src/dashboard/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+
+    name = models.CharField(max_length=255, primary_key=True)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    
+    product_id = models.AutoField(primary_key=True)
+    generic_name = models.CharField(max_length=255)
+    brand_name = models.CharField(max_length=255)
+    manufacturer = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.generic_name
+        
